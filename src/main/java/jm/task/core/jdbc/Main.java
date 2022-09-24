a8d1323ba2e85467e953f773b47c30288f32fd7b
Данил <81508158+DanilSuhanov@users.noreply.github.com>
2022-09-24 21:19:10 +0300

    24.09 - 21.19

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,32 +1,17 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            Util.init();
-            UserService userService = new UserServiceImpl();
-            userService.createUsersTable();
-            userService.saveUser("Name1", "LastName1", (byte) 20);
-            userService.saveUser("Name2", "LastName2", (byte) 21);
-            userService.saveUser("Name3", "LastName3", (byte) 22);
-            userService.getAllUsers().iterator().forEachRemaining(System.out :: println);
-            userService.cleanUsersTable();
-            userService.dropUsersTable();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                Util.closeConnection();
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Name1", "LastName1", (byte) 20);
+        userService.saveUser("Name2", "LastName2", (byte) 21);
+        userService.saveUser("Name3", "LastName3", (byte) 22);
+        userService.getAllUsers().iterator().forEachRemaining(System.out :: println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -2,22 +2,30 @@ package jm.task.core.jdbc.util;
 
 import java.sql.*;
 
-public class Util {
+public class Util implements AutoCloseable {
 
     private static final String url = "jdbc:mysql://localhost/test";
     private static final String username = "root";
     private static final String password = "kadjitnichegonekral";
 
     private static Connection con;
-    private static Statement statement;
 
-    public static void init() throws SQLException {
-        con = DriverManager.getConnection(url, username, password);
-        statement = con.createStatement();
+    static {
+        try {
+            con = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void closeConnection() throws SQLException {
-        con.close();
+    private static Statement statement;
+
+    static {
+        try {
+            statement = con.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void runUp(String sql) throws SQLException {
@@ -26,5 +34,10 @@ public class Util {
 
     public static ResultSet runQuery(String sql) throws SQLException {
         return statement.executeQuery(sql);
+    }
+
+    @Override
+    public void close() throws Exception {
+        con.close();
     }
 }
